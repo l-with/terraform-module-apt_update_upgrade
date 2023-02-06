d418a4c1899b8cc8ed06fe80755180e2282c4ede
Lars With <lars@with.de>
2023-02-06 19:15:36 +0100

    fix

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
 data "system_command" "apt_update" {
-  command = "apt update"
+  command = "apt-get update"
 }
 
 data "system_command" "apt_upgrade" {
   depends_on = [data.system_command.apt_update]
-  command    = "apt ugrade"
-  expect {
-    exit_code = 100
-  }
+  command    = "apt-get upgrade"
 }
 
 data "system_command" "apt_autoremove" {
   depends_on = [data.system_command.apt_upgrade]
   count      = var.auto_remove ? 1 : 0
-  command    = "apt autoremove"
+  command    = "apt-get autoremove"
 }
