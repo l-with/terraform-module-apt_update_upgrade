eb6ece2e6e2f3f1d86a9aae77298a2c6288f0208
Lars With <lars@with.de>
2023-02-06 19:40:00 +0100

    add -y

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 data "system_command" "apt_update" {
-  command = "apt-get update"
+  command = "apt-get update -y"
 }
 
 data "system_command" "apt_upgrade" {
   depends_on = [data.system_command.apt_update]
-  command    = "apt-get upgrade"
+  command    = "apt-get upgrade -y"
 }
 
 data "system_command" "apt_autoremove" {
   depends_on = [data.system_command.apt_upgrade]
   count      = var.auto_remove ? 1 : 0
-  command    = "apt-get autoremove"
+  command    = "apt-get autoremove -y"
 }
